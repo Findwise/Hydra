7ede30bfda3ebc0c6e072efcbcc2968ddd5056a7
Joel Westberg <joel.westberg@findwise.com>
2012-07-09 17:24:22 +0200

    Changes to ease implementation another backend

--- database/src/main/java/com/findwise/hydra/DatabaseDocument.java
+++ database/src/main/java/com/findwise/hydra/DatabaseDocument.java
@@ -3,5 +3,11 @@ package com.findwise.hydra;
 import com.findwise.hydra.common.Document;
 
 public interface DatabaseDocument<T extends DatabaseType> extends Document {
+
 	Object putMetadataField(String key, Object value);
+	
+	boolean touchedBy(String stage);
+	
+	boolean fetchedBy(String stage);
+	
 }
