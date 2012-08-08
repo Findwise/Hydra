dc8101267aeafdd877786df81fab62380500b1bc
Roar Granevang <roar.granevang@findwise.com>
2012-08-08 11:07:25 +0200

    added an variable in tikastage, simplefetching that supports addMetaData = true/false. Defaults to true so to not break backwards compability

--- stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/utils/TikaUtils.java
+++ stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/utils/TikaUtils.java
@@ -24,74 +24,75 @@ import com.findwise.hydra.common.Logger;
 import com.findwise.hydra.local.LocalDocument;
 
 public class TikaUtils {
-	public static void enrichDocumentWithFileContents(LocalDocument doc,
-			String fieldPrefix, InputStream stream, Parser parser) throws IOException,
-			SAXException, TikaException {
-		Metadata metadata = new Metadata();
-		ParseContext parseContext = new ParseContext();
-		parseContext.set(Parser.class, parser);
-		StringWriter textData = new StringWriter();
-		parser.parse(stream, new BodyContentHandler(textData), metadata,
-				parseContext);
 
-		addTextToDocument(doc, fieldPrefix, textData);
-		addMetadataToDocument(doc, fieldPrefix, metadata);
+    public static void enrichDocumentWithFileContents(LocalDocument doc,
+            String fieldPrefix, InputStream stream, Parser parser, boolean addMetaData) throws IOException,
+            SAXException, TikaException {
+        Metadata metadata = new Metadata();
+        ParseContext parseContext = new ParseContext();
+        parseContext.set(Parser.class, parser);
+        StringWriter textData = new StringWriter();
+        parser.parse(stream, new BodyContentHandler(textData), metadata,
+                parseContext);
 
-	}
+        addTextToDocument(doc, fieldPrefix, textData);
+        if (addMetaData) {
+            addMetadataToDocument(doc, fieldPrefix, metadata);
+        }
 
-	public static void addTextToDocument(LocalDocument doc, String fieldPrefix,
-			StringWriter textData) {
-		doc.putContentField(fieldPrefix + "content", textData.toString());
-	}
+    }
 
-	public static void addMetadataToDocument(LocalDocument doc, String fieldPrefix,
-			Metadata metadata) {
-		for (String name : metadata.names()) {
-			if (metadata.getValues(name).length > 1) {
-				doc.putContentField(fieldPrefix + name,
-						Arrays.asList(metadata.getValues(name)));
-			} else {
-				doc.putContentField(fieldPrefix + name, metadata.get(name));
-			}
-		}
-	}
-	
+    public static void addTextToDocument(LocalDocument doc, String fieldPrefix,
+            StringWriter textData) {
+        doc.putContentField(fieldPrefix + "content", textData.toString());
+    }
 
-	public static Set<URL> getUrlsFromObject(Object urlsObject) throws MalformedURLException {
-		if (urlsObject instanceof String) {
-			return new HashSet<URL>(Arrays.asList(new URL((String)urlsObject)));
-		} 
-		if (urlsObject instanceof Iterable<?>) {
-			Set<URL> urls = new HashSet<URL>();
-			for (Object urlObj : (Iterable<?>)urlsObject) {
-				urls.addAll(getUrlsFromObject(urlObj));
-			}
-			return urls;
-		}
-		return new HashSet<URL>();
-	}
-	
-	public static Map<String, Object> getFieldMatchingPattern(LocalDocument doc,
-			String pattern) {
-		Map<String, Object> fieldToUrl = new HashMap<String, Object>();
+    public static void addMetadataToDocument(LocalDocument doc, String fieldPrefix,
+            Metadata metadata) {
+        for (String name : metadata.names()) {
+            if (metadata.getValues(name).length > 1) {
+                doc.putContentField(fieldPrefix + name,
+                        Arrays.asList(metadata.getValues(name)));
+            } else {
+                doc.putContentField(fieldPrefix + name, metadata.get(name));
+            }
+        }
+    }
 
-		for (String field : doc.getContentFields()) {
-			Pattern p = Pattern.compile(pattern);
-			Matcher m = p.matcher(field);
-			if (m.matches()) {
-				String toField;
-				if (m.groupCount() >= 1) {
-					toField = m.group(1);
-				} else {
-					toField = m.group();
-				}
-				Logger.debug("Added " + doc.getContentField(field) + " to "
-						+ toField);
-				fieldToUrl.put(toField, doc.getContentField(field));
-			}
-		}
+    public static Set<URL> getUrlsFromObject(Object urlsObject) throws MalformedURLException {
+        if (urlsObject instanceof String) {
+            return new HashSet<URL>(Arrays.asList(new URL((String) urlsObject)));
+        }
+        if (urlsObject instanceof Iterable<?>) {
+            Set<URL> urls = new HashSet<URL>();
+            for (Object urlObj : (Iterable<?>) urlsObject) {
+                urls.addAll(getUrlsFromObject(urlObj));
+            }
+            return urls;
+        }
+        return new HashSet<URL>();
+    }
 
-		return fieldToUrl;
-	}
+    public static Map<String, Object> getFieldMatchingPattern(LocalDocument doc,
+            String pattern) {
+        Map<String, Object> fieldToUrl = new HashMap<String, Object>();
 
+        for (String field : doc.getContentFields()) {
+            Pattern p = Pattern.compile(pattern);
+            Matcher m = p.matcher(field);
+            if (m.matches()) {
+                String toField;
+                if (m.groupCount() >= 1) {
+                    toField = m.group(1);
+                } else {
+                    toField = m.group();
+                }
+                Logger.debug("Added " + doc.getContentField(field) + " to "
+                        + toField);
+                fieldToUrl.put(toField, doc.getContentField(field));
+            }
+        }
+
+        return fieldToUrl;
+    }
 }
