90ae5fe812264f7bf0f9f7d714c1f6d47cc11029
Roar Granevang <roar.granevang@findwise.com>
2012-09-12 14:02:49 +0200

    moved some stage files from the gard project to the common repo. These include RemoveFields, HTMLUnescaper and PrioritiezedCopyStage

---
+++ stages/processing/basic/src/test/java/com/findwise/hydra/stage/PrioritizedCopyStageTest.java
@@ -0,0 +1,199 @@
+package com.findwise.hydra.stage;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+import com.findwise.hydra.local.LocalDocument;
+import com.findwise.hydra.stage.ProcessException;
+import java.util.LinkedList;
+import java.util.List;
+import junit.framework.Assert;
+
+public class PrioritizedCopyStageTest {
+    @Test
+    public void testConditionalCopyFalse() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+        s.setConditionField("type");
+        s.setConditionValue("Person Profile");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("type", "SomethingWrong");
+        ld.putContentField("b", "bb");
+        ld.putContentField("c", "cc");
+        s.setConditionField("type");
+        s.setConditionValue("Person Profile");
+        s.process(ld);
+
+        if (ld.getContentField("a") != null) {
+            fail();
+        }
+    }
+    @Test
+    public void testConditionalCopyTrue() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+        s.setConditionField("type");
+        s.setConditionValue("Person Profile");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("type", "Person Profile");
+        ld.putContentField("b", "bb");
+        ld.putContentField("c", "cc");
+        s.setConditionField("type");
+        s.setConditionValue("Person Profile");
+        s.process(ld);
+
+        if (!ld.getContentField("a").equals("bb")) {
+            fail();
+        }
+    }
+    @Test
+    public void testOverwriteEmptyString() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("copyThis");
+        input.add("copyThis2");
+
+        s.setInputFields(input);
+        s.setOutputField("emptyfield");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("emptyfield", "");
+        ld.putContentField("copyThis", "stuff_to_be_copied");
+        ld.putContentField("copyThis2", "more_stuff_to_be_copied");
+        
+        s.process(ld);
+
+        Assert.assertEquals("stuff_to_be_copied", ld.getContentField("emptyfield"));
+    }
+    
+    @Test
+    public void testCopyAlreadyExist() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("a", "a");
+        s.process(ld);
+
+        if (!ld.getContentField("a").equals("a")) {
+            fail();
+        }
+    
+
+    }
+
+    @Test
+    public void testCopyAlreadyButOverWriteIsTrue() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("add1");
+        input.add("add2");
+
+        s.setInputFields(input);
+        s.setOutputField("out1");
+        
+        s.setOverWrite(true);
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("out1", "this_should_be_overwritten");
+        ld.putContentField("add1", "this_is_added");
+        ld.putContentField("add2", "this_is_not_added_bacause found in 1st");
+        s.process(ld);
+
+        Assert.assertEquals("this_is_added", ld.getContentField("out1"));
+    
+
+    }
+    
+    @Test
+    public void testFoundInFirst() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("b", "bb");
+        ld.putContentField("c", "cc");
+        s.process(ld);
+
+        if (!ld.getContentField("a").equals("bb")) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testFoundInSecond() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("c", "cc");
+        s.process(ld);
+
+        if (!ld.getContentField("a").equals("cc")) {
+            fail();
+        }
+    }
+    
+        @Test
+    public void testNotFound() throws ProcessException {
+
+        PrioritizedCopyStage s = new PrioritizedCopyStage();
+
+        List<String> input = new LinkedList<String>();
+        input.add("b");
+        input.add("c");
+
+        s.setInputFields(input);
+        s.setOutputField("a");
+
+        LocalDocument ld = new LocalDocument();
+        ld.putContentField("x", "cc");
+        s.process(ld);
+
+        if (ld.getContentField("a") != null) {
+            fail();
+        }
+    }
+        
+}
