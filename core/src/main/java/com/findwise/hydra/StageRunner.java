88558a6135096f7efd002271b2f5580b52ed76e9
Sture Svensson <sture.svensson@findwise.com>
2012-09-28 09:56:26 +0200

    created a new stagerunner that works in windows

--- core/src/main/java/com/findwise/hydra/StageRunner.java
+++ core/src/main/java/com/findwise/hydra/StageRunner.java
@@ -14,236 +14,216 @@ import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.ProcessDestroyer;
 import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.exec.launcher.CommandLauncher;
+import org.apache.commons.exec.launcher.CommandLauncherFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StageRunner extends Thread {
-	private StoredStage stage;
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	private StageDestroyer stageDestroyer;
-	private int timesToRetry;
-	private int timesStarted;
-	private boolean loggingEnabled;
-	private String jvmParameters;
-	private String startupArgsString;
-	
-	private boolean wasKilled;
-	
-	private boolean hasQueried = false;
-	
-	private int pipelinePort;
-	
-	public synchronized void setHasQueried() {
-		hasQueried = true;
-	}
-	
-	public synchronized boolean hasQueried() {
-		return hasQueried;
-	}
 
-	/**
-	 * Expected exit value should be 143 on *NIX systems, the exit value on a
-	 * TERM command
-	 */
-	/** Checked but not conclusively verified to be 143 on Windows as well. */
-	public static final int TERM_EXITCODE = 143;
+    private StoredStage stage;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private StageDestroyer stageDestroyer;
+    private int timesToRetry;
+    private int timesStarted;
+    private boolean loggingEnabled;
+    private String jvmParameters;
+    private String startupArgsString;
+    private boolean hasQueried = false;
+    private int pipelinePort;
 
-	public StageRunner(StoredStage stage, int pipelinePort) {
-		this.stage = stage;
-		this.pipelinePort = pipelinePort;
-		timesStarted = 0;
-		setParameters();
-		wasKilled = false;
-	}
+    public synchronized void setHasQueried() {
+        hasQueried = true;
+    }
 
-	public final void setParameters() {
-		Map<String, Object> conf = stage.getProperties();
-		if (conf.containsKey("jvm_parameters")) {
-			jvmParameters = (String) conf.get("jvm_parameters");
-		}
-		else {
-			jvmParameters = null;
-		}
-		if (conf.containsKey("retries")) {
-			timesToRetry = (Integer) conf.get("retries");
-		}
-		else {
-			timesToRetry = -1;
-		}
-		if (conf.containsKey("logging_enabled")) {
-			loggingEnabled = (Boolean) conf.get("logging_enabled");
-		}
-		else {
-			loggingEnabled = true;
-		}
-		if (conf.containsKey("cmdline_args")) {
-			startupArgsString = (String) conf.get("cmdline_args");
-		}
-		else {
-			startupArgsString = null;
-		}
-	}
+    public synchronized boolean hasQueried() {
+        return hasQueried;
+    }
+    /**
+     * Expected exit value should be 143 on *NIX systems, the exit value on a
+     * TERM command
+     */
+    /** Checked but not conclusively verified to be 143 on Windows as well. */
+    public static final int TERM_EXITCODE = 143;
 
-	public void run() {
+    public StageRunner(StoredStage stage, int pipelinePort) {
+        this.stage = stage;
+        this.pipelinePort = pipelinePort;
+        timesStarted = 0;
+        setParameters();
+    }
 
-		do {
-			logger.info("Starting stage " + stage.getName()
-					+ ". Times started so far: " + timesStarted);
-			timesStarted++;
-			boolean cleanShutdown = startStage();
-			if (cleanShutdown) {
-				return;
-			} 
-			if(!hasQueried()) {
-				logger.error("The stage "+stage.getName()+" did not start. It will not be restarted until configuration changes.");
-				return;
-			}
-		} while (timesToRetry == -1 || timesToRetry >= timesStarted);
+    public final void setParameters() {
+        Map<String, Object> conf = stage.getProperties();
+        if (conf.containsKey("jvm_parameters")) {
+            jvmParameters = (String) conf.get("jvm_parameters");
+        } else {
+            jvmParameters = null;
+        }
+        if (conf.containsKey("retries")) {
+            timesToRetry = (Integer) conf.get("retries");
+        } else {
+            timesToRetry = -1;
+        }
+        if (conf.containsKey("logging_enabled")) {
+            loggingEnabled = (Boolean) conf.get("logging_enabled");
+        } else {
+            loggingEnabled = true;
+        }
+        if (conf.containsKey("cmdline_args")) {
+            startupArgsString = (String) conf.get("cmdline_args");
+        } else {
+            startupArgsString = null;
+        }
+    }
 
-		logger.error("Stage " + stage.getName()
-				+ " has failed and cannot be restarted. ");
-	}
-	
-	public void printJavaVersion() {
-		CommandLine cmdLine = new CommandLine("java");
-		cmdLine.addArgument("-version");
-		DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
-		Executor executor = new DefaultExecutor();
-		try {
-			executor.execute(cmdLine, resultHandler);
-		} catch (ExecuteException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public void run() {
 
-	private boolean startStage() {
-		CommandLine cmdLine = new CommandLine("java");
-		cmdLine.addArgument(jvmParameters, false);
-		cmdLine.addArgument("-jar");
-		cmdLine.addArgument("${file}");
-		cmdLine.addArgument(stage.getName());
-		cmdLine.addArgument("localhost");
-		cmdLine.addArgument(""+pipelinePort);
-		cmdLine.addArgument(startupArgsString);
-		HashMap<String, File> map = new HashMap<String, File>();
-		map.put("file", stage.getFile().getAbsoluteFile());
-		cmdLine.setSubstitutionMap(map);
-		logger.debug("Launching with command " + cmdLine.toString());
-		DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+        do {
+            logger.info("Starting stage " + stage.getName()
+                    + ". Times started so far: " + timesStarted);
+            timesStarted++;
+            boolean cleanShutdown = startStage();
+            if (cleanShutdown) {
+                return;
+            }
+            if (!hasQueried()) {
+                logger.error("The stage " + stage.getName() + " did not start. It will not be restarted until configuration changes.");
+                return;
+            }
+        } while (timesToRetry == -1 || timesToRetry >= timesStarted);
 
-		Executor executor = new DefaultExecutor();
+        logger.error("Stage " + stage.getName()
+                + " has failed and cannot be restarted. ");
+    }
 
-		/** Ensure this process is stopped if the main JVM dies */
-		stageDestroyer = new StageDestroyer();
-		executor.setProcessDestroyer(stageDestroyer);
-		executor.setExitValue(TERM_EXITCODE);
+    public void printJavaVersion() {
+        CommandLine cmdLine = new CommandLine("java");
+        cmdLine.addArgument("-version");
+        DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+        Executor executor = new DefaultExecutor();
+        try {
+            executor.execute(cmdLine, resultHandler);
+        } catch (ExecuteException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		if (loggingEnabled) {
-			// Set up our generic logging service
-			PumpStreamHandler streamHandler = new PumpStreamHandler(
-					new StreamLogger(stage.getName()));
-			executor.setStreamHandler(streamHandler);
-		}
+    private boolean startStage() {
+        CommandLine cmdLine = new CommandLine("java");
+        cmdLine.addArgument(jvmParameters, false);
+        cmdLine.addArgument("-jar");
+        cmdLine.addArgument("${file}");
+        cmdLine.addArgument(stage.getName());
+        cmdLine.addArgument("localhost");
+        cmdLine.addArgument("" + pipelinePort);
+        cmdLine.addArgument(startupArgsString);
+        HashMap<String, File> map = new HashMap<String, File>();
+        map.put("file", stage.getFile().getAbsoluteFile());
+        cmdLine.setSubstitutionMap(map);
+        logger.info("Launching with command " + cmdLine.toString());
 
-		try {
-			executor.execute(cmdLine, resultHandler);
-		} catch (ExecuteException e) {
-			logger.error("Unable to execute " + cmdLine.toString(), e);
-		} catch (IOException e) {
-			logger.error("Unable to execute " + cmdLine.toString(), e);
-		}
+		// This code will do the stuff the executor-class does, but manually since for some reason it did not work in windows	
+        stageDestroyer = new StageDestroyer();
+        CommandLauncher cl = CommandLauncherFactory.createVMLauncher();
+        try {
+            //Give an empty enviorments-map.
+            Process p = cl.exec(cmdLine,(Map)null);
 
-		/** Let's wait and see if the process exits */
+            if (loggingEnabled) {
+                PumpStreamHandler streams = new PumpStreamHandler(new StreamLogger(stage.getName()));
+                streams.setProcessInputStream(p.getOutputStream());
+                streams.setProcessErrorStream(p.getErrorStream());
+                streams.setProcessOutputStream(p.getInputStream());
+                streams.start();
+            }
+            stageDestroyer.add(p);
+            
+            /** Wait untill the process exits */
+            p.waitFor();
 
-		try {
-			resultHandler.waitFor();
-		} catch (InterruptedException e) {
-			logger.error("resultHandler was interrupted", e);
-		}
+        } catch (Exception ex) {
+            //I stopped, restart me if possible!! 
+        }
 
-		int exitValue = resultHandler.getExitValue();
-		if (!wasKilled && executor.isFailure(exitValue)) {
-			logger.error("Stage " + stage.getName()
-					+ " terminated unexpectedly with exit value " + exitValue);
-			/** The stage crashed. Restart if we have restarts left */
-			return false;
-		}
-		return true;
-	}
+        logger.error("Stage " + stage.getName()
+                + " terminated unexpectedly");
+        /** The stage crashed. Restart if we have restarts left */
+        return false;
+    }
 
-	/**
-	 * Destroys the JVM running this stage. Should a JVM shutdown fail, it will
-	 * throw an IllegalStateException.
-	 */
-	public void destroy() {
-		logger.debug("Attempting to destroy JVM running stage "
-				+ stage.getName());
-		boolean success = stageDestroyer.killAll();
-		if (success) {
-			logger.debug("... destruction successful");
-		} else {
-			logger.error("JVM running stage " + stage.getName()
-					+ " was not killed");
-			throw new IllegalStateException("Orphaned process for "
-					+ stage.getName());
-		}
-		wasKilled = true;
-	}
+    /**
+     * Destroys the JVM running this stage. Should a JVM shutdown fail, it will
+     * throw an IllegalStateException.
+     */
+    public void destroy() {
+        logger.debug("Attempting to destroy JVM running stage "
+                + stage.getName());
+        boolean success = stageDestroyer.killAll();
+        if (success) {
+            logger.debug("... destruction successful");
+        } else {
+            logger.error("JVM running stage " + stage.getName()
+                    + " was not killed");
+            throw new IllegalStateException("Orphaned process for "
+                    + stage.getName());
+        }
+    }
 
-	/**
-	 * Manages the destruction of any Stages launched in this wrapper.
-	 * Automatically binds to the Runtime to shut down along with the master
-	 * JVM.
-	 * 
-	 * @author joel.westberg
-	 * 
-	 */
-	static class StageDestroyer extends Thread implements ProcessDestroyer {
-		private final List<Process> processes = new ArrayList<Process>();
+    /**
+     * Manages the destruction of any Stages launched in this wrapper.
+     * Automatically binds to the Runtime to shut down along with the master
+     * JVM.
+     * 
+     * @author joel.westberg
+     * 
+     */
+    static class StageDestroyer extends Thread implements ProcessDestroyer {
 
-		@Override
-		public boolean add(Process p) {
-			/**
-			 * Register this destroyer to Runtime in order to avoid orphaned
-			 * processes if the main JVM dies
-			 */
-			Runtime.getRuntime().addShutdownHook(this);
-			processes.add(p);
-			return true;
-		}
+        private final List<Process> processes = new ArrayList<Process>();
 
-		@Override
-		public boolean remove(Process p) {
-			return processes.remove(p);
-		}
+        @Override
+        public boolean add(Process p) {
+            /**
+             * Register this destroyer to Runtime in order to avoid orphaned
+             * processes if the main JVM dies
+             */
+            Runtime.getRuntime().addShutdownHook(this);
+            processes.add(p);
+            return true;
+        }
 
-		@Override
-		public int size() {
-			return processes.size();
-		}
+        @Override
+        public boolean remove(Process p) {
+            return processes.remove(p);
+        }
 
-		/**
-		 * Invoked by the runtime ShutdownHook on JVM exit
-		 */
-		public void run() {
-			killAll();
-		}
+        @Override
+        public int size() {
+            return processes.size();
+        }
 
-		public boolean killAll() {
-			boolean success = true;
-			synchronized (processes) {
-				for (Process process : processes) {
-					try {
-						process.destroy();
-					} catch (RuntimeException t) {
-						success = false;
-					}
-				}
-			}
-			return success;
-		}
+        /**
+         * Invoked by the runtime ShutdownHook on JVM exit
+         */
+        public void run() {
+            killAll();
+        }
 
-	}
+        public boolean killAll() {
+            boolean success = true;
+            synchronized (processes) {
+                for (Process process : processes) {
+                    try {
+                        process.destroy();
+                    } catch (RuntimeException t) {
+                        success = false;
+                    }
+                }
+            }
+            return success;
+        }
+    }
 }
