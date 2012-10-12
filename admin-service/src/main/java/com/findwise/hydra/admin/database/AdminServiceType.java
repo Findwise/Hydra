71b53edea74472c6d319516f390eddaa1a54fd72
Simon Stenstrom <simon.stenstrom@findwise.com>
2012-10-12 16:29:19 +0200

    Added implementation of interface DatabaseQuery to be used for querying and DatabaseType

---
+++ admin-service/src/main/java/com/findwise/hydra/admin/database/AdminServiceType.java
@@ -0,0 +1,12 @@
+package com.findwise.hydra.admin.database;
+
+import com.findwise.hydra.DatabaseType;
+
+/**
+ * Empty interface to be implemented by all classes using AdminService
+ * @author simon.stenstrom
+ *
+ */
+public interface AdminServiceType extends DatabaseType {
+
+}
