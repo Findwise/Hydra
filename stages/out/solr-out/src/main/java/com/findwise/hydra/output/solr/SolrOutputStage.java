22222684822637470470f5e9c58ab848eb826666
Roar Granevang <roar.granevang@findwise.com>
2012-11-06 15:15:56 +0100

    changed action check so both action.add and action.update adds the document

--- stages/out/solr-out/src/main/java/com/findwise/hydra/output/solr/SolrOutputStage.java
+++ stages/out/solr-out/src/main/java/com/findwise/hydra/output/solr/SolrOutputStage.java
@@ -42,12 +42,10 @@ public class SolrOutputStage extends AbstractOutputStage {
 
 		try {
 			
-		if (action == Action.ADD) {
+		if (action == Action.ADD || action == Action.UPDATE) {
 			add(doc);
 		} else if (action == Action.DELETE) {
 			delete(doc);
-		} else if (action == Action.UPDATE) {
-			failDocument(doc, new RequiredArgumentMissingException("action UPDATE is not supported. Failing document"));
 		} else{
 			failDocument(doc, new RequiredArgumentMissingException("action not set in document. This document would never be sent to solr"));
 		}
