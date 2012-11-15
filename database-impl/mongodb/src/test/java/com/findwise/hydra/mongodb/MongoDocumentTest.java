a169fdd125f3ad6321e0f392d4642f22d5494436
Joel Westberg <joel.westberg@findwise.com>
2012-11-15 14:02:15 +0100

    Adding tests for removed fields

---
+++ database-impl/mongodb/src/test/java/com/findwise/hydra/mongodb/MongoDocumentTest.java
@@ -0,0 +1,24 @@
+package com.findwise.hydra.mongodb;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.findwise.hydra.local.LocalDocument;
+
+public class MongoDocumentTest {
+
+	@Test
+	public void testNullTransfer() throws Exception {
+		LocalDocument ld = new LocalDocument();
+		ld.putContentField("field", "value");
+		ld.markSynced();
+		ld.removeContentField("field");
+		
+		MongoDocument md = new MongoDocument(ld.toJson());
+		System.out.println(md);
+		
+		Assert.assertEquals(1, md.getTouchedContent().size());
+		Assert.assertNull(md.getContentField("field"));
+	}
+}
