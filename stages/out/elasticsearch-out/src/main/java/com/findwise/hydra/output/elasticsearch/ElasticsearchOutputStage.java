5f93fbfd96616ec6368b1b582c1970a24520a427
Olof Nilsson <olof.nilsson@findwise.com>
2012-11-16 14:29:15 +0100

    Integration test for ElasticsearchOutputStage that tests add and delete with an embedded es node. Use maven goal verify to run on build.

--- stages/out/elasticsearch-out/src/main/java/com/findwise/hydra/output/elasticsearch/ElasticsearchOutputStage.java
+++ stages/out/elasticsearch-out/src/main/java/com/findwise/hydra/output/elasticsearch/ElasticsearchOutputStage.java
@@ -21,6 +21,12 @@ import com.findwise.hydra.stage.Parameter;
 import com.findwise.hydra.stage.RequiredArgumentMissingException;
 import com.findwise.hydra.stage.Stage;
 
+/**
+ * Writes to elasticsearch via the Transport protocol.
+ * 
+ * @author olof.nilsson
+ *
+ */
 @Stage(description = "A stage that writes documents to elasticsearch")
 public class ElasticsearchOutputStage extends AbstractOutputStage {
 
@@ -29,29 +35,29 @@ public class ElasticsearchOutputStage extends AbstractOutputStage {
 
 	@Parameter(description = "Transport port of the elasticsearch nodes")
 	private int transportPort = 9300;
-	
+
 	@Parameter(description = "Name of the cluster to join")
 	private String clusterName = "elasticsearch";
-	
+
 	@Parameter(description = "Name of elasticsearch index")
 	private String documentIndex = "main";
-	
+
 	@Parameter(description = "Document type")
 	private String documentType = "default";
-	
+
 	@Parameter(description = "ID field in the document")
 	private String documentIdField = "docId";
-	
+
 	@Parameter(description = "Timeout for requests in millis")
 	private int requestTimeout = 10000;
-	
+
 	private Client client;
-	
+
 	@Override
 	public void init() throws RequiredArgumentMissingException {
 		client = constructClient();
 	}
-	
+
 	@Override
 	public void output(LocalDocument document) {
 		final Action action = document.getAction();
@@ -69,38 +75,35 @@ public class ElasticsearchOutputStage extends AbstractOutputStage {
 				update(document);
 				break;
 			default:
-				fail(document);
+				failDocument(document, new RequiredArgumentMissingException("Action must be ADD, DELETE or UPDATE."));
 				break;
 			}
 		} catch (ElasticSearchException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
+			failDocument(document, e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			failDocument(document, e);
 		}
 	}
-	
-	
+
 	private void update(LocalDocument document) throws ElasticSearchException, IOException {
 		add(document);
 	}
 
 	private void add(LocalDocument document) throws ElasticSearchException, IOException {
-		String docId = (String) document.getContentField(documentIdField);
+		String docId = getDocumentId(document);
 		String json = document.contentFieldsToJson(document.getContentFields());
-		Logger.debug("Indexing document to index " + documentIndex + " with type " + documentType);
+		Logger.debug("Indexing document " + getDocumentId(document) + " to index " + documentIndex + " with type " + documentType);
 		ListenableActionFuture<IndexResponse> actionFuture = client.prepareIndex(documentIndex, documentType, docId)
 			.setSource(json)
 			.execute();
 		IndexResponse response = actionFuture.actionGet(requestTimeout);
 		Logger.debug("Got response for docId " + response.getId());
+		accept(document);
 	}
-	
+
 	private void delete(LocalDocument document) throws IOException {
 		
-		String docId = (String) document.getContentField(documentIdField);
+		String docId = getDocumentId(document);
 		
 		ListenableActionFuture<DeleteResponse> actionFuture = client.prepareDelete(documentIndex, documentType, docId)
 				.execute();
@@ -110,6 +113,16 @@ public class ElasticsearchOutputStage extends AbstractOutputStage {
 		}
 		else {
 			Logger.debug("Deleted document with id " + response.getId());
+		}
+		accept(document);
+	}
+
+	private void failDocument(LocalDocument doc, Throwable reason) {
+		try {
+			Logger.error("Failing document " + doc.getID(), reason);
+			fail(doc, reason);
+		} catch (Exception e) {
+			Logger.error("Could not fail document with hydra id: " + doc.getID(), e);
 		}
 	}
 
@@ -123,11 +136,15 @@ public class ElasticsearchOutputStage extends AbstractOutputStage {
 		}
 		return tclient;
 	}
-	
+
+	protected String getDocumentId(LocalDocument document) {
+		return (String) document.getContentField(documentIdField);
+	}
+
 	public void setClient(Client client) {
 		this.client = client;
 	}
-	
+
 	public String getClusterName() {
 		return clusterName;
 	}
@@ -141,6 +158,19 @@ public class ElasticsearchOutputStage extends AbstractOutputStage {
 	}
 
 	public void setEsNodes(List<String> esNodes) {
-		this.esNodes = esNodes;
+		this.esNodes.clear();
+		this.esNodes.addAll(esNodes);
+	}
+
+	public String getIndex() {
+		return this.documentIndex;
+	}
+
+	public String getType() {
+		return this.documentType;
+	}
+
+	public String getIdField() {
+		return this.documentIdField;
 	}
 }
