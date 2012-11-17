f92688bd9aeaf9ff7de94327dd07620847aa78ae
Joel Westberg <joel.westberg@findwise.com>
2012-11-17 18:52:21 +0100

    Disallowing NUL in MongoDocuments documents

--- database-impl/mongodb/src/test/java/com/findwise/hydra/mongodb/MongoConnectorTest.java
+++ database-impl/mongodb/src/test/java/com/findwise/hydra/mongodb/MongoConnectorTest.java
@@ -10,8 +10,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,9 +24,11 @@ import org.junit.Test;
 import com.findwise.hydra.DatabaseDocument;
 import com.findwise.hydra.DatabaseQuery;
 import com.findwise.hydra.TestModule;
+import com.findwise.hydra.DatabaseConnector.ConversionException;
 import com.findwise.hydra.common.Document;
 import com.findwise.hydra.common.DocumentFile;
 import com.findwise.hydra.common.Document.Status;
+import com.findwise.hydra.local.LocalDocument;
 import com.google.inject.Guice;
 import com.mongodb.Mongo;
 
@@ -364,5 +369,41 @@ public class MongoConnectorTest {
 		}
 	}
 	
+	@Test(expected = ConversionException.class)
+	public void testConversionWithNullCharacterInFieldName() throws ConversionException {
+		LocalDocument ld = new LocalDocument();
+		ld.putContentField("field\0000name", "field value");
+		mdc.convert(ld);
+	}
+	
+	@Test(expected = ConversionException.class)
+	public void testConversionWithNullCharacterInList() throws ConversionException {
+		LocalDocument ld = new LocalDocument();
+		ld.putContentField("fieldname", Arrays.asList(new String[] {"some", "string", "with", "null\u0000here"}));
+		mdc.convert(ld);
+	}
 
+	@Test(expected = ConversionException.class)
+	public void testConversionWithNullCharacterInString() throws ConversionException {
+		LocalDocument ld = new LocalDocument();
+		ld.putContentField("fieldname", "some\u0000null");
+		mdc.convert(ld);
+	}
+
+	@Test(expected = ConversionException.class)
+	public void testConversionWithNullCharacterInMap() throws ConversionException {
+		LocalDocument ld = new LocalDocument();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("NUL \u0000 here", "");
+		ld.putContentField("field", map);
+		try {
+			mdc.convert(ld);
+		} catch (ConversionException e) {
+			map.clear();
+			map.put("nulvalue", "the \u0000 value");
+			ld.putContentField("field", map);
+			mdc.convert(ld);
+		}
+		fail("Was able to put in a map with a null in a field name");
+	}
 }
