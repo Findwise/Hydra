b91edc4d108515c9ea6ca76792b03884bd69890c
Joel Westberg <joel.westberg>
2012-11-29 15:57:53 +0100

    Adding some copying

--- sleepy-stage/src/main/java/com/findwise/hydra/stage/SleepyStage.java
+++ sleepy-stage/src/main/java/com/findwise/hydra/stage/SleepyStage.java
@@ -1,4 +1,6 @@
 package com.findwise.hydra.stage;
+import java.util.Random;
+
 import com.findwise.hydra.local.LocalDocument;
 import com.findwise.hydra.stage.AbstractProcessStage;
 import com.findwise.hydra.stage.Parameter;
@@ -10,6 +12,8 @@ public class SleepyStage extends AbstractProcessStage {
 	@Parameter
 	private int timeToSleep = 500;
 	
+	private Random r = new Random();
+	
 	@Override
 	public void process(LocalDocument doc) throws ProcessException {
 		try {
@@ -17,6 +21,9 @@ public class SleepyStage extends AbstractProcessStage {
 		} catch(InterruptedException e) {
 			throw new ProcessException(e);
 		}
+		doc.putContentField("sleepy", doc.getContentFields().toArray()[r.nextInt(doc.getContentFields().size())]);
+		doc.putContentField("sleepy2", doc.getContentFields().toArray()[r.nextInt(doc.getContentFields().size())]);
+		doc.putContentField("sleepy3", doc.getContentFields().toArray()[r.nextInt(doc.getContentFields().size())]);
 	}
 	
 }
