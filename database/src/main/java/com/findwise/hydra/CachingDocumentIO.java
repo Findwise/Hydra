e38ac45937e2c493aaf6576aa037a7c0e6e4853c
Joel Westberg <joel.westberg>
2012-12-20 11:22:05 +0100

    Oops, removing unused and unavaialable import that got added in through eclipse autoimport somehow :(

--- database/src/main/java/com/findwise/hydra/CachingDocumentIO.java
+++ database/src/main/java/com/findwise/hydra/CachingDocumentIO.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import org.objenesis.instantiator.SerializationInstantiatorHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
