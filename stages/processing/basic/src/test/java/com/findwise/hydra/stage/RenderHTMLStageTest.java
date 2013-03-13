d6a04c2d7663787388e801c6ea207cd1b68588a2
Roar Granevang <roar.granevang@findwise.com>
2013-03-13 12:59:20 +0100

    Added a stage that removes html and javascript from a html page

---
+++ stages/processing/basic/src/test/java/com/findwise/hydra/stage/RenderHTMLStageTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.findwise.hydra.stage;
+
+import com.findwise.hydra.local.LocalDocument;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
+import junit.framework.Assert;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+/**
+ *
+ * @author Roar Granevang
+ */
+public class RenderHTMLStageTest {
+
+	public RenderHTMLStageTest() {
+	}
+
+	@Test
+	public void testProcessEmptyField() throws Exception {
+		InputStream is = RenderHTMLStageTest.class.getResourceAsStream("/htmlDocument.html");
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(is, writer, "utf-8");
+		String htmlString = writer.toString();
+
+		RenderHTMLStage instance = new RenderHTMLStage();
+
+        List<String> inputFields = new LinkedList<String>();
+        inputFields.add("html");
+        inputFields.add("body");
+        instance.setFields(inputFields);
+
+		LocalDocument ld = new LocalDocument();
+		
+		instance.init();
+		instance.process(ld);
+
+		//Only really want to test that it doesn't throw a nullpointer when inputfields are empty
+		Assert.assertFalse(htmlString.equals(ld.getContentField("html")));
+		Assert.assertFalse(htmlString.equals(ld.getContentField("body")));
+	}
+
+	@Test
+	public void testProcessSettingOnlyOneInputField() throws Exception {
+		InputStream is = RenderHTMLStageTest.class.getResourceAsStream("/htmlDocument.html");
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(is, writer, "utf-8");
+		String htmlString = writer.toString();
+
+		RenderHTMLStage instance = new RenderHTMLStage();
+
+        List<String> inputFields = new LinkedList<String>();
+        inputFields.add("html");
+        inputFields.add("body");
+        instance.setFields(inputFields);
+
+		LocalDocument ld = new LocalDocument();
+		
+		
+		ld.putContentField("html", htmlString);
+		ld.putContentField("body", htmlString);
+
+		Assert.assertEquals(htmlString, ld.getContentField("html"));
+		Assert.assertEquals(htmlString, ld.getContentField("body"));
+
+		instance.init();
+		instance.process(ld);
+
+		Assert.assertFalse(htmlString.equals(ld.getContentField("html")));
+		Assert.assertFalse(htmlString.equals(ld.getContentField("body")));
+	}
+
+	@Test(expected = RequiredArgumentMissingException.class)  
+	public void testInputFieldNotSet() throws RequiredArgumentMissingException{
+		RenderHTMLStage instance = new RenderHTMLStage();
+		instance.init();
+	}
+}
