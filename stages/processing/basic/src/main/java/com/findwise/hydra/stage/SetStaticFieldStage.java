c3260fd9a0688adb620dbcfad232813304f32f0e
Martin Nycander <martin.nycander@findwise.com>
2013-09-17 16:24:19 +0200

    SetStaticFieldStage no longer checks for the mere existance of a field, but looks at the value to see if it is empty as well

--- stages/processing/basic/src/main/java/com/findwise/hydra/stage/SetStaticFieldStage.java
+++ stages/processing/basic/src/main/java/com/findwise/hydra/stage/SetStaticFieldStage.java
@@ -1,6 +1,7 @@
 package com.findwise.hydra.stage;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -8,32 +9,49 @@ import com.findwise.hydra.local.LocalDocument;
 
 /**
  * Adds a field with a specified value to the document.
- * 
+ *
  * @author joel.westberg
  */
 @Stage(description = "Modifies a field with a static value. Can append values to lists, and will create lists if configured to do so.")
 public class SetStaticFieldStage extends AbstractProcessStage {
-
-	public enum Policy {
+	public static enum Policy {
 		OVERWRITE, SKIP, THROW, ADD
-	};
+	}
 
-	@Parameter(required = true, name = "fieldNames", description = "A map of fields to modify, and the values to write to them")
+	@Parameter(required = true, name = "fieldNames",
+			description = "A map of fields to modify, and the values to write to them")
 	private Map<String, Object> fieldValueMap;
-	@Parameter(name = "overwritePolicy", description = "Switch for behaviour when modifying. Available options: 0/OVERWRITE = overwrite content, 1/SKIP = skip if there is content, 2/THROW = throw exception if there is content, 3/ADD = append to content, converting the content to a list if necessary (default)")
+	@Parameter(name = "overwritePolicy",
+			description = "Switch for behaviour when modifying. Available options: 0/OVERWRITE = overwrite content, 1/SKIP = skip if there is content, 2/THROW = throw exception if there is content, 3/ADD = append to content, converting the content to a list if necessary (default)")
 	private Policy overwritePolicy = Policy.ADD;
 
 	@Override
 	public void process(LocalDocument doc) throws ProcessException {
 		for (Map.Entry<String, Object> entry : fieldValueMap.entrySet()) {
-			if (!doc.hasContentField(entry.getKey()) || overwritePolicy == Policy.OVERWRITE) {
-				doc.putContentField(entry.getKey(), entry.getValue());
+			String key = entry.getKey();
+			if (missingContent(doc, key) || overwritePolicy == Policy.OVERWRITE) {
+				doc.putContentField(key, entry.getValue());
 			} else if (overwritePolicy == Policy.ADD) {
-				addValueToField(doc, entry.getKey(), entry.getValue());
+				addValueToField(doc, key, entry.getValue());
 			} else if (overwritePolicy == Policy.THROW) {
-				throw new ProcessException("Field " + entry.getKey()
+				throw new ProcessException("Field " + key
 						+ " already has a value!");
 			}
+		}
+	}
+
+	private boolean missingContent(LocalDocument doc, String key) {
+		if (!doc.hasContentField(key)) {
+			return true;
+		}
+
+		Object value = doc.getContentField(key);
+		if (value instanceof String) {
+			return ((String) value).isEmpty();
+		} else if (value instanceof Collection) {
+			return ((Collection) value).isEmpty();
+		} else {
+			return false;
 		}
 	}
 
