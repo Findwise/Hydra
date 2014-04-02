425a50dc38521710bba63f4b527a08c61ad07e62
Olof Nilsson <olof.nilsson@findwise.com>
2014-04-02 15:07:29 +0200

    Reformat

--- stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/SimpleFetchingTikaStage.java
+++ stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/SimpleFetchingTikaStage.java
@@ -24,64 +24,64 @@ import com.findwise.hydra.stage.RequiredArgumentMissingException;
 import com.findwise.hydra.stage.Stage;
 
 /**
- * Downloads content which should be parsed by Tika. A pattern is given which 
+ * Downloads content which should be parsed by Tika. A pattern is given which
  * points out all the fields containing URLs to download and parsed.
- * 
- * Notice that the stage will add a prefix to all parsed fields. The prefix has 
+ * <p/>
+ * Notice that the stage will add a prefix to all parsed fields. The prefix has
  * the following format FIRST_CAPTURING_CROUP + _ + PARSED_FIELD_NAME
- * 
- * e.g. If urlFieldPattern is set to crawl_(url) all parsed fields from tika 
+ * <p/>
+ * e.g. If urlFieldPattern is set to crawl_(url) all parsed fields from tika
  * will start with url_.
- * 
+ * <p/>
  * Sample configuration:
- {@code 
- {
-   "stageClass": "com.findwise.hydra.stage.tika.SimpleFetchingTikaStage",
-   "query": {
-     "equals":{"type": "file" },
-     "touched":{"set-internal-type": true}
-   }, 
-   "urlFieldPattern" : "(url)"   
-  }  
-  }
-  */
+ * {@code
+ * {
+ * "stageClass": "com.findwise.hydra.stage.tika.SimpleFetchingTikaStage",
+ * "query": {
+ * "equals":{"type": "file" },
+ * "touched":{"set-internal-type": true}
+ * },
+ * "urlFieldPattern" : "(url)"
+ * }
+ * }
+ */
 @Stage(description = "A stage that fetches the content from a given url and appends it the the document")
 public class SimpleFetchingTikaStage extends AbstractProcessStage {
     private static Logger logger = LoggerFactory.getLogger(SimpleFetchingTikaStage.class);
 
-	@Parameter(required = true, description = "The field name pattern that should be matched where " +
-			"urls will be found. First group plus \"_\" will be used as field prefix. Example:" +
-			" \"attachment_(.*)\" will match for example attachment_a and will use \"a_\" as prefix")
-	public String urlFieldPattern = null;
+    @Parameter(required = true, description = "The field name pattern that should be matched where " +
+            "urls will be found. First group plus \"_\" will be used as field prefix. Example:" +
+            " \"attachment_(.*)\" will match for example attachment_a and will use \"a_\" as prefix")
+    public String urlFieldPattern = null;
 
-	@Parameter(name = "addMetaData", description = "Add the metadata to the document or not. Defaults to true")
-	public boolean addMetaData = true;
+    @Parameter(name = "addMetaData", description = "Add the metadata to the document or not. Defaults to true")
+    public boolean addMetaData = true;
 
-	@Parameter(description = "Set to true, will also do language detection and add the field 'prefix_language' according to the prefix rules. Defaults to true")
-	public boolean addLanguage = true;
+    @Parameter(description = "Set to true, will also do language detection and add the field 'prefix_language' according to the prefix rules. Defaults to true")
+    public boolean addLanguage = true;
 
-	@Parameter(description = "Username for basic authentication.")
-	public String username = null;
+    @Parameter(description = "Username for basic authentication.")
+    public String username = null;
 
-	@Parameter(description = "Password for basic authentication.")
-	public String password = null;
+    @Parameter(description = "Password for basic authentication.")
+    public String password = null;
 
-	private Parser parser = new AutoDetectParser();
+    private Parser parser = new AutoDetectParser();
 
-	@Override
-	public void process(LocalDocument doc) throws Exception {
-		Map<String, Object> urls = FieldHelper.getFieldMatchingPattern(doc.getContentMap(),
+    @Override
+    public void process(LocalDocument doc) throws Exception {
+        Map<String, Object> urls = FieldHelper.getFieldMatchingPattern(doc.getContentMap(),
                 urlFieldPattern);
         UriParser uriParser = new UriParser();
         InputStreamParser inputStreamParser = new InputStreamParser(parser);
-		for (String field : urls.keySet()) {
-			Iterator<URL> it = uriParser.getUrlsFromObject(urls.get(field)).iterator();
-			for (int i = 1; it.hasNext(); i++) {
-				String num = (i > 1) ? "" + i : "";
-				URL url = it.next();
-				URLConnection connection = createConnection(url);
-				final InputStream inputStream = connection.getInputStream();
-				try {
+        for (String field : urls.keySet()) {
+            Iterator<URL> it = uriParser.getUrlsFromObject(urls.get(field)).iterator();
+            for (int i = 1; it.hasNext(); i++) {
+                String num = (i > 1) ? "" + i : "";
+                URL url = it.next();
+                URLConnection connection = createConnection(url);
+                final InputStream inputStream = connection.getInputStream();
+                try {
                     String prefix = field + num + "_";
                     ParsedData parsedData = inputStreamParser.parse(inputStream);
                     doc.putContentField(prefix + "content", parsedData.getContent());
@@ -95,45 +95,45 @@ public class SimpleFetchingTikaStage extends AbstractProcessStage {
                     if (addLanguage) {
                         doc.putContentField(prefix + "language", parsedData.getLanguage());
                     }
-				} finally {
-					inputStream.close();
-				}
-			}
-		}
-	}
+                } finally {
+                    inputStream.close();
+                }
+            }
+        }
+    }
 
-	private URLConnection createConnection(URL url) throws IOException {
-		URLConnection connection = url.openConnection();
-		if (useBasicAuthentication()) {
-			String authString = username + ":" + password;
-			byte[] authEncBytes = Base64.encodeBase64(authString
-					.getBytes("UTF-8"));
-			String authStringEnc = "Basic " + new String(authEncBytes, "UTF-8");
-			connection.setRequestProperty("Authorization", authStringEnc);
-		}
-		return connection;
-	}
+    private URLConnection createConnection(URL url) throws IOException {
+        URLConnection connection = url.openConnection();
+        if (useBasicAuthentication()) {
+            String authString = username + ":" + password;
+            byte[] authEncBytes = Base64.encodeBase64(authString
+                    .getBytes("UTF-8"));
+            String authStringEnc = "Basic " + new String(authEncBytes, "UTF-8");
+            connection.setRequestProperty("Authorization", authStringEnc);
+        }
+        return connection;
+    }
 
-	private boolean useBasicAuthentication() {
-		return username != null && password != null;
-	}
+    private boolean useBasicAuthentication() {
+        return username != null && password != null;
+    }
 
-	@Override
-	public void init() throws RequiredArgumentMissingException {
-		if (urlFieldPattern == null) {
-			throw new RequiredArgumentMissingException(
-					"Missing parameter urlFieldPattern");
-		}
-		logger.debug("Initiated SimpleTikaStage");
-	}
+    @Override
+    public void init() throws RequiredArgumentMissingException {
+        if (urlFieldPattern == null) {
+            throw new RequiredArgumentMissingException(
+                    "Missing parameter urlFieldPattern");
+        }
+        logger.debug("Initiated SimpleTikaStage");
+    }
 
 	/* For testing purposes */
 
-	void setUrlFieldPattern(String urlFieldPattern) {
-		this.urlFieldPattern = urlFieldPattern;
-	}
+    void setUrlFieldPattern(String urlFieldPattern) {
+        this.urlFieldPattern = urlFieldPattern;
+    }
 
-	void setParser(Parser parser) {
-		this.parser = parser;
-	}
+    void setParser(Parser parser) {
+        this.parser = parser;
+    }
 }
--- stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/TikaStage.java
+++ stages/processing/tika/src/main/java/com/findwise/hydra/stage/tika/TikaStage.java
@@ -21,21 +21,21 @@ import com.findwise.hydra.stage.Stage;
 /**
  * @author jwestberg
  */
-@Stage(description="Stage that fetches any files attached to the document being processed and parses them with Tika. Any fields found by Tika will be stored in <filename>_*")
+@Stage(description = "Stage that fetches any files attached to the document being processed and parses them with Tika. Any fields found by Tika will be stored in <filename>_*")
 public class TikaStage extends AbstractProcessStage {
     @Parameter(name = "addMetaData", description = "Add the metadata to the document or not. Defaults to true")
     private boolean addMetaData = true;
 
-	@Parameter(description = "Set to true, will also do language detection and add the field 'prefix_language' according to the prefix rules. Defaults to true")
-	private boolean addLanguage = true;
-    
-	static private Parser parser = new AutoDetectParser();
+    @Parameter(description = "Set to true, will also do language detection and add the field 'prefix_language' according to the prefix rules. Defaults to true")
+    private boolean addLanguage = true;
 
-	@Override
-	public void process(LocalDocument doc) throws TikaException, SAXException, IOException {
-		List<String> files = doc.getFileNames();
-		for(String fileName : files) {
-			DocumentFile<Local> df = doc.getFile(fileName);
+    static private Parser parser = new AutoDetectParser();
+
+    @Override
+    public void process(LocalDocument doc) throws TikaException, SAXException, IOException {
+        List<String> files = doc.getFileNames();
+        for (String fileName : files) {
+            DocumentFile<Local> df = doc.getFile(fileName);
             String prefix = fileName.replace('.', '_') + "_";
             InputStreamParser inputStreamParser = new InputStreamParser(parser);
             ParsedData parsedData = inputStreamParser.parse(df.getStream());
@@ -51,23 +51,23 @@ public class TikaStage extends AbstractProcessStage {
             if (addLanguage) {
                 doc.putContentField(prefix + "language", parsedData.getLanguage());
             }
-		}
-	}
+        }
+    }
 
-	public boolean isAddMetaData() {
-		return addMetaData;
-	}
+    public boolean isAddMetaData() {
+        return addMetaData;
+    }
 
-	public void setAddMetaData(boolean addMetaData) {
-		this.addMetaData = addMetaData;
-	}
+    public void setAddMetaData(boolean addMetaData) {
+        this.addMetaData = addMetaData;
+    }
 
-	public boolean isAddLanguage() {
-		return addLanguage;
-	}
+    public boolean isAddLanguage() {
+        return addLanguage;
+    }
 
-	public void setAddLanguage(boolean addLanguage) {
-		this.addLanguage = addLanguage;
-	}
+    public void setAddLanguage(boolean addLanguage) {
+        this.addLanguage = addLanguage;
+    }
 
 }
