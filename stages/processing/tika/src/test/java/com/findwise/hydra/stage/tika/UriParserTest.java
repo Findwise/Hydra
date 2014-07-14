e3e2c70ff887ca93640ffaa8c0a141d82253e025
Andreas Salomonsson <andreas.salomonsson@findwise.com>
2014-07-14 13:55:27 +0200

    UriParser now handles URLs with @ in the path.

--- stages/processing/tika/src/test/java/com/findwise/hydra/stage/tika/UriParserTest.java
+++ stages/processing/tika/src/test/java/com/findwise/hydra/stage/tika/UriParserTest.java
@@ -1,14 +1,16 @@
 package com.findwise.hydra.stage.tika;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class UriParserTest {
 
@@ -23,22 +25,22 @@ public class UriParserTest {
     public void testURLEncoding() throws Exception {
         String path = "/some spaces in url/and query";
         URI uri = parser.uriFromString("https://user:password@google.com:8080" +
-                path + "?q=some space&some other space#anchor");
+                path + "?q=some space&some%20other%20space#anchor");
 
-        Assert.assertEquals(8080, uri.getPort());
-        Assert.assertEquals("google.com", uri.getHost());
-        Assert.assertEquals("https", uri.getScheme());
-        Assert.assertEquals(path, uri.getPath());
-        Assert.assertEquals(path.replace(" ", "%20"), uri.getRawPath());
+        assertEquals(8080, uri.getPort());
+        assertEquals("google.com", uri.getHost());
+        assertEquals("https", uri.getScheme());
+        assertEquals(path, uri.getPath());
+        assertEquals(path.replace(" ", "%20"), uri.getRawPath());
     }
 
     @Test
     public void testGetUrlFromString() throws Exception {
         List<URL> urls = parser.getUrlsFromObject("http://google.com");
 
-        Assert.assertEquals(1, urls.size());
+        assertEquals(1, urls.size());
         for (URL url : urls) {
-            Assert.assertEquals("http://google.com", url.toString());
+            assertEquals("http://google.com", url.toString());
         }
     }
 
@@ -47,14 +49,28 @@ public class UriParserTest {
         List<String> exp = Arrays.asList("http://google.com", "http://dn.se");
         List<URL> urls = parser.getUrlsFromObject(exp);
 
-        Assert.assertEquals(exp.size(), urls.size());
+        assertEquals(exp.size(), urls.size());
         for (URL url : urls) {
-            Assert.assertTrue(exp.contains(url.toString()));
+            assertTrue(exp.contains(url.toString()));
         }
     }
 
-    @Test(expected = URISyntaxException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testGetUrlFromIncorrectString() throws Exception {
         parser.getUrlsFromObject("a");
+    }
+
+    @Test
+    public void testUrlWithAtCharacterInThePath() throws Exception {
+        String path = "http://hostname:9080/groups/scs/@scswithdrawn/documents/scs/mdaw/mzm5/~edisp/scs_0000377_99.pdf";
+        URI uri = parser.uriFromString(path);
+        assertEquals("http", uri.getScheme());
+        assertEquals("hostname:9080", uri.getAuthority());
+        assertNull(uri.getUserInfo());
+        assertEquals("hostname", uri.getHost());
+        assertEquals(9080, uri.getPort());
+        assertEquals("/groups/scs/@scswithdrawn/documents/scs/mdaw/mzm5/~edisp/scs_0000377_99.pdf", uri.getPath());
+        assertNull(uri.getQuery());
+        assertNull(uri.getFragment());
     }
 }
