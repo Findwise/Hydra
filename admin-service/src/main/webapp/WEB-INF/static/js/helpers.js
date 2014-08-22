dbfc69f1ed33b2f60dae04a8b729ca189cfd4c8c
Olof Nilsson <olof.nilsson@findwise.com>
2014-08-22 11:51:29 +0200

    Fixes rendering issues with upload timestamps and stagegroups with multiple stages

--- admin-service/src/main/webapp/WEB-INF/static/js/helpers.js
+++ admin-service/src/main/webapp/WEB-INF/static/js/helpers.js
@@ -73,7 +73,11 @@ function prettyPrint(value) {
         return new Handlebars.SafeString(value);
     } else if (typeof value == 'object') {
         if(value.value){
-            return new Handlebars.SafeString(JSON.stringify(value.value));
+            if (value.type == 'String') {
+                return new Handlebars.SafeString(value.value);
+            } else {
+                return new Handlebars.SafeString(JSON.stringify(value.value));
+            }
         } else {
              return new Handlebars.SafeString('-');
         }
@@ -110,7 +114,8 @@ function printProperties(key, value) {
 }
 
 function msToReadable(ms) {
-    x = ms / 1000;
+    var x = new Date() - new Date(ms);
+    x /= 1000;
     seconds = Math.round(x % 60);
     x /= 60;
     minutes = Math.round(x % 60);
