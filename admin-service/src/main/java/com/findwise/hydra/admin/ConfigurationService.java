4afd2f0350f271563242f38e8bafa5d9873aff05
Olof Nilsson <olof.nilsson@findwise.com>
2014-08-22 12:45:41 +0200

    Fixes stage properties being overwritten in stage groups
    When fetching a stage group, if the stages had parameters with the same name, the last stage would overwrite the properties of other stages with the same parameter.

--- admin-service/src/main/java/com/findwise/hydra/admin/ConfigurationService.java
+++ admin-service/src/main/java/com/findwise/hydra/admin/ConfigurationService.java
@@ -3,6 +3,7 @@ package com.findwise.hydra.admin;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.concurrent.ExecutionException;
 import com.findwise.hydra.DatabaseException;
 import com.findwise.hydra.PipelineStatus;
 import com.findwise.hydra.Stage;
+import com.findwise.hydra.StageGroup;
 import com.findwise.hydra.admin.rest.StageClassNotFoundException;
 import com.findwise.hydra.stage.AbstractStage;
 import com.google.common.cache.Cache;
@@ -135,22 +137,18 @@ public class ConfigurationService<T extends DatabaseType> {
 	 * @param stage a stage configuration
 	 */
 	@SuppressWarnings("unchecked")
-	public void addStageParameters(Stage stage) throws DatabaseException, StageClassNotFoundException {
+	public void addStageParameters(Stage stage, String stageGroupName) throws DatabaseException, StageClassNotFoundException {
 		try {
-			Map<String, StageInformation> stages = getStagesMap(stage.getDatabaseFile());
-			Map<String, Object> properties = stage.getProperties();
+			final Map<String, StageInformation> stages = getStagesMap(stage.getDatabaseFile());
+			final Map<String, Object> properties = stage.getProperties();
 			String stageClass = (String) properties.get(AbstractStage.ARG_NAME_STAGE_CLASS);
 			if (null != stageClass && stages.containsKey(stageClass)) {
 				Map<String, Object> parameters = (Map<String, Object>) stages.get(stageClass).get("parameters");
 				for (String parameterName : parameters.keySet()) {
-					Map<String, Object> parameter = (Map<String, Object>) parameters.get(parameterName);
-					if (properties.containsKey(parameterName)) {
-						parameter.put("value", properties.get(parameterName));
-					}
-					properties.put(parameterName, parameter);
+					addParameter(properties, parameters, parameterName);
 				}
 				properties.put("stageName", stage.getName());
-				properties.put("stageGroup", connector.getPipelineReader().getPipeline().getGroupForStage(stage.getName()).getName());
+				properties.put("stageGroup", stageGroupName);
 				properties.put("libId", stage.getDatabaseFile().getId());
 			} else {
 				throw new StageClassNotFoundException("Stage class '" + stageClass
@@ -160,6 +158,16 @@ public class ConfigurationService<T extends DatabaseType> {
 		} catch (IOException e) {
 			throw new DatabaseException("Failed to scan pipeline", e);
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void addParameter(Map<String, Object> properties, Map<String, Object> parameters, String parameterName) {
+		Map<String, Object> parameter = new HashMap<String, Object>();
+		parameter.putAll((Map<String, Object>) parameters.get(parameterName));
+		if (properties.containsKey(parameterName)) {
+			parameter.put("value", properties.get(parameterName));
+		}
+		properties.put(parameterName, parameter);
 	}
 
 	/**
